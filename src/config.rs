c2c3c6418f2a4abe5e15d1f7fe0145f0b9fe0c98
Jeffas <dev@jeffas.io>
2019-07-01 23:01:19 +0100

    Add config struct

---
+++ src/config.rs
@@ -0,0 +1,11 @@
+/// A struct for holding the config
+pub struct Config {
+    /// The density to generate blocks at
+    pub block_density: f64,
+    /// The strength of each block
+    pub block_strength: u16,
+    /// The width of the bar
+    pub bar_width: u16,
+    /// The strength of the ball
+    pub ball_power: u16,
+}
